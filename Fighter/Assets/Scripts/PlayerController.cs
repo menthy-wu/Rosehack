916e7f56fb9a91d3b272c8fa3a1ada8a8a4ce45e
Joshua <joshuahsu108@gmail.com>
2023-01-14 13:39:18 -0800

    Add hitting and dummy enemy

--- Fighter/Assets/Scripts/PlayerController.cs
+++ Fighter/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
     public bool isJump = false;
     public float jumpForce = 20f;
     public float moveSpeed = 5f;
+    public Vector2 knockbackForce = new Vector2(5f, 15f);
+    public bool facingRight = true;
+    public GameObject enemy = null;
+    public KeyCode jumpKey = KeyCode.W;
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+    public KeyCode attackKey = KeyCode.Space;
+    public bool isAttacking = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,25 +25,60 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && !isJump)
+        // face enemy
+        if (enemy != null)
+        {
+            if (enemy.transform.position.x > transform.position.x)
+            {
+                facingRight = true;
+            }
+            else
+            {
+                facingRight = false;
+            }
+        }
+
+        if (Input.GetKey(jumpKey) && !isJump)
         {
             // Set velocity to jumpForce
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpForce);
             // Print message to console
             Debug.Log("Jump!");
         }
-        if (Input.GetKey(KeyCode.D) && !isJump)
+        if (Input.GetKey(rightKey))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
-        else if (Input.GetKey(KeyCode.A) && !isJump)
+        else if (Input.GetKey(leftKey))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
-        else if (!isJump)
+        else
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
         }
+
+        // Hitting enemy with spacebar
+        if (Input.GetKey(KeyCode.Space) && !isAttacking)
+        {
+            // Get all colliders in a 1 unit radius in front of the player
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position + transform.right * (facingRight ? 1 : -1), 1f);
+            // Loop through all colliders
+            foreach (Collider2D collider in hitColliders)
+            {
+                // If the collider is the enemy
+                if (collider.gameObject == enemy)
+                {
+                    // Send message to enemy to take damage and direction
+                    int[] parameters = new int[] { 1, facingRight ? 1 : 0 };
+                    collider.gameObject.SendMessage("TakeDamage", parameters);
+                    isAttacking = true;
+                    attackCooldown();
+                }
+            }
+        }
+
+
 
     }
 
@@ -54,5 +97,28 @@ public class PlayerController : MonoBehaviour
             isJump = true;
             Debug.Log("Air!");
         }
+    }
+    void TakeDamage(int[] parameters)
+    {
+        int damage = parameters[0];
+        bool facingRight = parameters[1] == 1;
+        Debug.Log("Enemy took " + damage + " damage!");
+        if (facingRight)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-knockbackForce.x, knockbackForce.y);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(knockbackForce.x, knockbackForce.y);
+        }
+    }
+    void attackCooldown()
+    {
+        StartCoroutine(attackCooldownCoroutine());
+    }
+    IEnumerator attackCooldownCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isAttacking = false;
     }
 }
