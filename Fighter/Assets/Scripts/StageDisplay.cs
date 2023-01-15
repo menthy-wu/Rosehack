e2dc304cad1cca5e7ca9b34957c77d0cc79948eb
Joshua <joshuahsu108@gmail.com>
2023-01-14 19:29:25 -0800

    Complete moveset and health damage

---
+++ Fighter/Assets/Scripts/StageDisplay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageDisplay : MonoBehaviour
+{
+    GameObject hub;
+    GameObject src;
+    GameObject bell;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setStage(string stage)
+    {
+        
+    }
+}
